1ec963562f428536d70716a11d6e033eeba38b6a
Feri <feri.ferdiana@adiwisista.com>
2024-11-06 18:09:29 +0700

    done about us web

--- en/about-us/history.php
+++ en/about-us/history.php
@@ -32,13 +32,10 @@
                   min-[412px]:hidden 
                   min-[390px]:hidden
                   min-[320px]:hidden">
-        <div>
-          <img class="w-[100%] h-[auto]" src="/bumi-kadaka/resources/img/about-us/main-about-us.png" alt="">
+        <div class="bg-[url('../../resources/img/about-us/main-about-us.png')] mt-[125px] w-[100%] h-[550px]">
         </div>
-        <div class="bg-[#F1AE93] w-[263px] h-[78px]">
-          <div class="pt-[15px] mt-[-78px]">
+        <div class="bg-[#F1AE93] w-[263px] mt-[-45px]">
             <span class="text-white text-[30px] font-bold">About US</span>
-          </div>
         </div>
 
         <div class="m-[50px]">
@@ -46,8 +43,156 @@
             <span class="font-bold text-[40px]">History</span>
           </div>
 
-          <div class="flex">
+          <div class="flex
+                      xl:mt-[50px]
+                      lg:mt-[50px]">
+            <div class="w-[45%]">
+              <img src="/bumi-kadaka/resources/img/about-us/history-content-1.png" 
+                      class="lg:w-[475px] lg:h-[396px]">
+            </div>
+            <div class="w-[55%] text-justify
+                        lg:font-[700]
+                        xl:font-[700]">
+              <p>
+                PT Bumi Kadaka, established in 2013 in Bandung, Indonesia, focuses on hotel, resort, culinary, and tourism services. The company combines local uniqueness, natural beauty, technology, and sustainable practices to drive innovation in the hospitality and tourism industry. Sit suspendisse ultricies et phasellus tincidunt. Urna morbi felis nisl pharetra suspendisse nec malesuada ipsum augue. Sapien lacus vestibulum elementum mi tristique. Gravida elit massa vitae integer in pulvinar quis. Aliquet pharetra elementum amet bibendum in. Tellus dictum sagittis aliquet pretium ipsum. Viverra nulla eleifend tellus ut quis at.
+              </p>
+            </div>
           </div>
+
+          <div class="flex
+                      xl:mt-[50px]
+                      lg:mt-[50px]">
+              <div class="w-[55%] text-justify
+                          lg:font-[700] lg:mr-[60px]
+                          xl:font-[700] xl:mr-[60px]">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur. Quis libero mi dictumst egestas mattis pretium habitasse non. Neque mauris lorem gravida et nec lacus aenean feugiat vestibulum. Tincidunt porta aenean mi diam mi in. Integer eget mauris purus placerat. Non ut amet vel lectus et sed ut integer purus. Et consectetur vitae commodo tellus eu metus tempus sagittis ultrices. In eu ac eget ac. Condimentum faucibus facilisis vitae at volutpat nunc. Sit suspendisse ultricies et phasellus tincidunt. Urna morbi felis nisl pharetra suspendisse nec malesuada ipsum augue. Sapien lacus vestibulum elementum mi tristique. Gravida elit massa vitae integer in pulvinar quis. Aliquet pharetra elementum amet bibendum in. Tellus dictum sagittis aliquet pretium ipsum. Viverra nulla eleifend tellus ut quis at.
+                </p>
+              </div>
+              <div class="w-[40%]">
+                <img src="/bumi-kadaka/resources/img/about-us/history-content-2.png" 
+                        class="lg:w-[475px] lg:h-[396px]">
+            </div>        
+          </div>
+
+          <div class="bg-[#307060]
+                      xl:mt-[50px] xl:h-[27px] xl:w-[100%]
+                      lg:mt-[50px] lg:h-[27px] lg:w-[100%]">
+          </div>
+
+          <div class="xl:mt-[50px]
+                      lg:mt-[50px]">
+              <div class="text-left mb-[50px]">
+                <span class="font-bold text-[40px]">Milestone</span>
+              </div>
+
+              <div class="text-justify font-bold">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur. Libero eget placerat in lectus nunc a. Lectus phasellus ornare eget a arcu. Elementum leo aenean et aliquam commodo elit volutpat sagittis. Non magna sed sit pellentesque dolor ut quisque mauris. Urna hendrerit imperdiet sapien commodo egestas at interdum volutpat.
+                </p>
+              </div>
+          </div>
+
+          <div class="flex
+                      xl:mt-[50px]
+                      lg:mt-[50px]">
+            <div class="xl:w-[45%]
+                        lg:w-[45%]">
+                <img src="/bumi-kadaka/resources/img/about-us/present.png" alt=""
+                     class="xl:h-[730px]
+                            lg:h-[730px]">
+            </div>
+            <div class="xl:w-[55%]
+                        lg:w-[55%]">
+              <div class="font-[700] text-left
+                          xl:text-[40px]
+                          lg:text-[40px]">
+                <label>PAST - PRESENT</label>
+              </div>
+
+              <div class="mt-[20px] text-left flex">
+                <div class="mr-[50px] font-[600] 
+                            xl:text-[32px] xl:h-[80px] xl:mt-[15px] 
+                            lg:text-[32px] xl:h-[80px] xl:mt-[15px]">
+                  <label>2013</label>
+                </div>
+                <div class="mr-[50px]
+                            xl:xl:h-[80px] xl:mt-[25px] xl:text-[20px]
+                            lg:xl:h-[80px] lg:mt-[25px] lg:text-[20px]">
+                  <label>The company was established</label>
+                </div>
+              </div>
+
+              <div class="mt-[5px] w-[100%]" style="border-bottom : 2px solid black"></div>
+
+              <div class="mt-[20px] text-left flex">
+                <div class="mr-[50px] font-[600] 
+                            xl:text-[32px] xl:h-[80px] xl:mt-[15px] 
+                            lg:text-[32px] xl:h-[80px] xl:mt-[15px]">
+                  <label>2016</label>
+                </div>
+                <div class="mr-[50px]
+                            xl:xl:h-[80px] xl:mt-[10px] xl:text-[20px]
+                            lg:xl:h-[80px] lg:mt-[10px] lg:text-[20px]">
+                  <label>The first business unit, Villa Ethnic "Rumah Forest Hills," commenced operations.</label>
+                </div>
+              </div>
+
+              <div class="mt-[20px] w-[100%]" style="border-bottom : 2px solid black"></div>
+
+              <div class="mt-[20px] text-left flex">
+                <div class="mr-[50px] font-[600] 
+                            xl:text-[32px] xl:h-[80px] xl:mt-[15px] 
+                            lg:text-[32px] xl:h-[80px] xl:mt-[15px]">
+                  <label>2018</label>
+                </div>
+                <div class="mr-[50px]
+                            xl:xl:h-[80px] xl:mt-[25px] xl:text-[20px]
+                            lg:xl:h-[80px] lg:mt-[25px] lg:text-[20px]">
+                  <label>Starting building Forest Hills Hotel Ciwidey</label>
+                </div>
+              </div>
+
+              <div class="mt-[5px] w-[100%]" style="border-bottom : 2px solid black"></div>
+
+              <div class="mt-[20px] text-left flex">
+                <div class="mr-[50px] font-[600] 
+                            xl:text-[32px] xl:h-[80px] xl:mt-[15px] 
+                            lg:text-[32px] xl:h-[80px] xl:mt-[15px]">
+                  <label>2023</label>
+                </div>
+                <div class="mr-[50px]
+                            xl:xl:h-[80px] xl:mt-[25px] xl:text-[20px]
+                            lg:xl:h-[80px] lg:mt-[25px] lg:text-[20px]">
+                  <label>Forest Hills Hotel - Ciwidey started its operations</label>
+                </div>
+              </div>
+
+              <div class="mt-[5px] w-[100%]" style="border-bottom : 2px solid black"></div>
+
+              <div class="mt-[20px] text-left flex">
+                <div class="mr-[50px] font-[600] 
+                            xl:text-[32px] xl:h-[80px] xl:mt-[15px] 
+                            lg:text-[32px] xl:h-[80px] xl:mt-[15px]">
+                  <label>2024</label>
+                </div>
+                <div class="mr-[50px]
+                            xl:xl:h-[80px] xl:mt-[25px] xl:text-[20px]
+                            lg:xl:h-[80px] lg:mt-[25px] lg:text-[20px]">
+                  <label>Starting extend building 10 rooms and ballroom</label>
+                </div>
+              </div>
+
+              <div class="mt-[5px] w-[100%]" style="border-bottom : 2px solid black"></div>
+
+            </div>
+          </div>
+
+          <div class="bg-[#307060]
+                      xl:mt-[50px] xl:h-[27px] xl:w-[100%]
+                      lg:mt-[50px] lg:h-[27px] lg:w-[100%]">
+          </div>
+
         </div>
 
     </div>
